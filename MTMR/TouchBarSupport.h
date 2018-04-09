b69cdd5d607396dcf2a685fd14427ae7a33c7862
Toxblh <toxblh@gmail.com>
2018-04-09 01:14:37 +0100

    Added full work buttons! Added Time on touchbar. Next plan: first plugins, and custom buttons

---
+++ MTMR/TouchBarSupport.h
@@ -0,0 +1,13 @@
+//
+//  TouchBarSupport.h
+//  MTMR
+//
+//  Created by Anton Palgunov on 08/04/2018.
+//  Copyright © 2018 Anton Palgunov. All rights reserved.
+//
+
+#ifndef TouchBarSupport_h
+#define TouchBarSupport_h
+
+
+#endif /* TouchBarSupport_h */
---
+++ MTMR/TouchBarSupport.m
@@ -0,0 +1,9 @@
+//
+//  TouchBarSupport.m
+//  MTMR
+//
+//  Created by Anton Palgunov on 08/04/2018.
+//  Copyright © 2018 Anton Palgunov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>

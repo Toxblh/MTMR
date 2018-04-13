0e07f714581f3090c92d7e13c2fe893875384f27
Toxblh <toxblh@gmail.com>
2018-04-13 22:14:38 +0100

    Scripts for create dmg

--- build.sh
+++ build.sh
@@ -1,1 +1,19 @@
-xcodebuild -project MTMR.xcodeproj -target MTMR -configuration Release
+NAME='MTMR'
+
+rm -r Release 2>/dev/null
+
+xcodebuild archive \
+	-scheme "$NAME" \
+	-archivePath Release/App.xcarchive
+
+xcodebuild \
+	-exportArchive \
+	-archivePath Release/App.xcarchive \
+	-exportOptionsPlist export-options.plist \
+	-exportPath Release
+
+cd Release
+rm -r App.xcarchive
+
+# Prerequisite: npm i -g create-dmg
+create-dmg "${NAME}.app"

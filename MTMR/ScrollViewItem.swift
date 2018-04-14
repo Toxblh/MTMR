78509fb84defbc09e4a7d57b2d9674d50a585eeb
Toxblh <toxblh@gmail.com>
2018-04-14 02:17:01 +0100

    Less spacing in scroll

--- MTMR/ScrollViewItem.swift
+++ MTMR/ScrollViewItem.swift
@@ -6,6 +6,7 @@ class ScrollViewItem: NSCustomTouchBarItem {
         super.init(identifier: identifier)
         let views = items.compactMap { $0.view }
         let stackView = NSStackView(views: views)
+        stackView.spacing = 1
         stackView.orientation = .horizontal
         let scrollView = NSScrollView(frame: CGRect(origin: .zero, size: stackView.fittingSize))
         scrollView.documentView = stackView

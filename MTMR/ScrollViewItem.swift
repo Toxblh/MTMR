479190d12b3a36c69246cdf56ca942bf6928d747
Toxblh <toxblh@gmail.com>
2018-04-27 23:38:13 +0100

    fix center line for apple script

--- MTMR/ScrollViewItem.swift
+++ MTMR/ScrollViewItem.swift
@@ -10,7 +10,7 @@ class ScrollViewItem: NSCustomTouchBarItem {
         stackView.orientation = .horizontal
         let scrollView = NSScrollView(frame: CGRect(origin: .zero, size: stackView.fittingSize))
         scrollView.documentView = stackView
-        scrollView.documentView?.bounds.origin = CGPoint(x: 0.0, y: -5.5)
+        scrollView.documentView?.bounds.origin = CGPoint(x: 0.0, y: -2.5)
         self.view = scrollView
     }
     

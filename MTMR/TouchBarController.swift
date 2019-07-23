6a85bea5b58e06c29a389fd0f9e5bf6fdeb35511
bobrosoft <bobrosoft@yandex.ru>
2019-07-23 17:07:47 +0400

    Add new "yandexWeather" widget

--- MTMR/TouchBarController.swift
+++ MTMR/TouchBarController.swift
@@ -35,6 +35,8 @@ extension ItemType {
             return "com.toxblh.mtmr.brightness"
         case .weather(interval: _, units: _, api_key: _, icon_type: _):
             return "com.toxblh.mtmr.weather"
+        case .yandexWeather(interval: _):
+            return "com.toxblh.mtmr.yandexWeather"
         case .currency(interval: _, from: _, to: _, full: _):
             return "com.toxblh.mtmr.currency"
         case .inputsource():
@@ -269,6 +271,8 @@ class TouchBarController: NSObject, NSTouchBarDelegate {
             }
         case let .weather(interval: interval, units: units, api_key: api_key, icon_type: icon_type):
             barItem = WeatherBarItem(identifier: identifier, interval: interval, units: units, api_key: api_key, icon_type: icon_type)
+        case let .yandexWeather(interval: interval):
+            barItem = YandexWeatherBarItem(identifier: identifier, interval: interval)
         case let .currency(interval: interval, from: from, to: to, full: full):
             barItem = CurrencyBarItem(identifier: identifier, interval: interval, from: from, to: to, full: full)
         case .inputsource():
